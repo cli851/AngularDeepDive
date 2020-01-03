94a0a46c9c38bba9958585ba83a7f3ed64c7462c
cli851 <can_li@comcast.com>
2020-01-02 20:52:29 -0500

    12

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,31 @@
+import { Course } from './../model/course';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+
+  @Input()
+  course: Course
+
+  @Input()
+  cardIndex: number
+
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>()
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onCourseViewed() {
+    console.log("card component")
+
+    this.courseEmitter.emit(this.course)
+  }
+
+}
